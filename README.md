f7a8b9e128b3442791fd389ccfc257067685d2af
Andrew Mackenzie <andrew@mackenzie-serres.net>
2017-11-19 18:53:32 +0100

    Implement multi-threading

--- README.md
+++ README.md
@@ -30,7 +30,7 @@ However, you can have them run using:
 ## Benchmarks
 Run on:
 - MacBook Pro (Retina, 13-inch, Late 2013)
-- Processor 2.4GHz Intel Core i5
+- Processor 2.4GHz Intel Core i5  (that should be 4 cores, not HyperThreading, I understand)
 - Memory 16 GB 1600 MHz DDR3
 - macOS High Sierra
 
@@ -62,3 +62,37 @@ multiplies and adds and just incrementing an offset into the pixel buffer.
 bench: 502,205,421 ns/iter (+/- 92,783,074)
 bench: 511,686,945 ns/iter (+/- 62,674,916)
 bench: 504,082,705 ns/iter (+/- 21,991,316)
+
+### v2.0.0 - Multithreaded version using crossbeam crate (as per book)
+In this version I have updated the core to match latest version of the book, 
+and implemented multi-threading using crossbeam.
+
+Let's experiment with the number of threads to see how it varies (on this MBP):
+ 1: 6.02 real         5.91 user         0.05 sys  
+ 2: 5.42 real         5.74 user         0.07 sys  
+ 3: 4.23 real         5.74 user         0.04 sys  
+ 4: 3.22 real         5.68 user         0.03 sys  
+ 5: 2.64 real         5.69 user         0.03 sys  
+ 6: 2.54 real         5.70 user         0.05 sys
+ 7: 2.12 real         5.73 user         0.04 sys  
+ 8: 1.92 real         5.76 user         0.03 sys  
+ 9: 1.86 real         5.79 user         0.03 sys  
+10: 1.75 real         5.80 user         0.03 sys  <<<<<
+11: 1.76 real         5.85 user         0.03 sys  
+12: 1.85 real         5.83 user         0.05 sys  
+ 
+Using multi-threading the real (wall-clock) time has been reduced from 6.2s to 1.75s, 
+or 3.5 times speedup
+
+It's interesting that on a 4-core (non-HyperThreaded) machine that the optimal number of cores
+is 10... But there's a bit of fixed (single-threaded) time in writing the file. 
+
+To get to the optimal number for the rendering part, in next part I will refactor render to
+take number of threads as a parameter and write benchmark tests for the different values.
+
+Here is a set of baseline bench numbers for the different functions before in future veersions
+I play with them and multi-threading in render more.
+
+bench_escapes             ... bench:          32 ns/iter (+/- 8)  
+bench_pixel_to_point      ... bench:           0 ns/iter (+/- 0)  
+bench_render_1000_by_1000 ... bench: 493,086,861 ns/iter (+/- 12,176,118)  
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,18 @@
 #![feature(test)]
 
 extern crate num;
+
 use num::Complex;
 
 extern crate image;
+
 use image::ColorType;
 use image::png::PNGEncoder;
 
 extern crate test;
 extern crate dir_diff;
 extern crate tempdir;
+extern crate crossbeam;
 
 use std::fs::File;
 use std::path::PathBuf;
@@ -27,13 +30,35 @@ fn main() {
     }
 
     let filename = PathBuf::from(&args[1]);
+
     let bounds = parse_pair(&args[2], 'x').expect("error parsing image dimensions");
-    let upper_left = parse_pair(&args[3], ',').expect("error parsing upper left corner point");
-    let lower_right = parse_pair(&args[4], ',').expect("error parsing lower right corner point");
+    let upper_left = parse_complex(&args[3]).expect("error parsing upper left corner point");
+    let lower_right = parse_complex(&args[4]).expect("error parsing lower right corner point");
 
     let mut pixels = vec![0; bounds.0 * bounds.1];
 
-    render(&mut pixels, bounds, upper_left, lower_right);
+    // TODO note that the output file changes when # threads is > 1 and is even !!!! Sounds like a bug to me
+    let threads = 1;
+    let rows_per_band = match bounds.1 % threads {
+        0 => bounds.1 / threads,
+        _ => bounds.1 / threads + 1
+    };
+
+    {
+        let bands: Vec<&mut [u8]> = pixels.chunks_mut(rows_per_band * bounds.0).collect();
+        crossbeam::scope(|spawner| {
+            for (i, band) in bands.into_iter().enumerate() {
+                let top = rows_per_band * i;
+                let height = band.len() / bounds.0;
+                let band_bounds = (bounds.0, height);
+                let band_upper_left = pixel_to_point(bounds, (0, top), upper_left, lower_right);
+                let band_lower_right = pixel_to_point(bounds, (bounds.0, top + height), upper_left, lower_right);
+                spawner.spawn(move || {
+                    render(band, band_bounds, band_upper_left, band_lower_right);
+                });
+            }
+        });
+    }
 
     write_bitmap(&filename, &pixels, bounds).expect("error writing PNG file");
 }
@@ -44,11 +69,11 @@ fn main() {
 /// by 'T::from_str'.
 /// If 's' has the proper form, return 'Some<(x,y)>'.
 /// If 's' doesn't parse correctly, return None.
-fn parse_pair<T:FromStr>(s: &str, separator: char) -> Option<(T,T)> {
+fn parse_pair<T: FromStr>(s: &str, separator: char) -> Option<(T, T)> {
     match s.find(separator) {
         None => None,
         Some(index) => {
-            match (T::from_str(&s[..index]), T::from_str(&s[index+1..])) {
+            match (T::from_str(&s[..index]), T::from_str(&s[index + 1..])) {
                 (Ok(l), Ok(r)) => Some((l, r)),
                 _ => None
             }
@@ -56,19 +81,35 @@ fn parse_pair<T:FromStr>(s: &str, separator: char) -> Option<(T,T)> {
     }
 }
 
-/// Return the point on the complex plane corresponding to a given pixel in the bitmap
-/// 'bounds' is a pair giving the width and height of the bitmap.
-/// 'pixel' is a pair indicating a particular pixel in the bitmap.
-/// 'upper_left' and 'upper_right' are points on the complex plane designating the area our
-/// bitmap covers.
-fn pixel_to_point(bounds: (usize, usize), pixel: (usize, usize),
-                  upper_left: (f64, f64), lower_right: (f64, f64)) -> (f64, f64) {
-    // it might be nicer to find the position on the middle of the pixel, instead of the
-    // upper left corner, but this is easier to write tests for
-    let (width, height) = (lower_right.0 - upper_left.0, upper_left.1 - lower_right.1);
+/// Parse a pair of floating-point numbers separated by a comma as a complex /// number.
+fn parse_complex(s: &str) -> Option<Complex<f64>> {
+    match parse_pair(s, ',') {
+        Some((re, im)) => Some(Complex { re, im }),
+        None => None
+    }
+}
 
-    (upper_left.0 + pixel.0 as f64 * width  / bounds.0 as f64,
-     upper_left.1 - pixel.1 as f64 * height / bounds.1 as f64)
+/// Given the row and column of a pixel in the output image, return the
+/// corresponding point on the complex plane.
+///
+/// `bounds` is a pair giving the width and height of the image in pixels.
+/// `pixel` is a (row, column) pair indicating a particular pixel in that image.
+/// The `upper_left` and `lower_right` parameters are points on the complex
+/// plane designating the area our image covers.
+fn pixel_to_point(bounds: (usize, usize), pixel: (usize, usize),
+                  upper_left: Complex<f64>,
+                  lower_right: Complex<f64>) -> Complex<f64>
+{
+    // Extract these two calculations outside the function and calling loop
+    let width = lower_right.re - upper_left.re;
+    let height = upper_left.im - lower_right.im;
+
+    Complex {
+        re: upper_left.re + (pixel.0 as f64 * (width / bounds.0 as f64)),
+        im: upper_left.im - (pixel.1 as f64 * (height / bounds.1 as f64))
+        // This is subtraction as pixel.1 increases as we go down,
+        // but the imaginary component increases as we go up.
+    }
 }
 
 /// Try to determine if 'c' is in the Mandlebrot set, using at most 'limit' iterations to decide
@@ -76,8 +117,10 @@ fn pixel_to_point(bounds: (usize, usize), pixel: (usize, usize),
 /// to leave the circle of radius two centered on the origin.
 /// If 'c' seems to be a member (more precisely, if we reached the iteration limit without being
 /// able to prove that 'c' is not a member) return 'None'
-fn escapes(c : Complex<f64>, limit: u32) -> Option<u32> {
-    let mut z = Complex{ re: 0.0, im: 0.0 };
+fn escapes(c: Complex<f64>, limit: u32) -> Option<u32> {
+    let mut z = Complex { re: 0.0, im: 0.0 };
+    // TODO just assign z to c at start then loop from 1, as first time around result will always be c
+    // check that if c > 4.0 then just return count 0?
     for i in 0..limit {
         z = z * z + c;
         if z.norm_sqr() > 4.0 {
@@ -93,15 +136,15 @@ fn escapes(c : Complex<f64>, limit: u32) -> Option<u32> {
 /// grayscale pixel per byte. The 'upper_left' and 'lower_right' arguments specify points on the
 /// complex plane corresponding to the upper left and lower right corners of the pixel buffer.
 fn render(pixels: &mut [u8], bounds: (usize, usize),
-          upper_left: (f64, f64), lower_right: (f64, f64)) {
+          upper_left: Complex<f64>, lower_right: Complex<f64>) {
     assert_eq!(pixels.len(), bounds.0 * bounds.1);
 
-    let mut offset : usize = 0;
-    for r in 0..bounds.1 {
-        for c in 0..bounds.0 {
-            let point = pixel_to_point(bounds, (c,r), upper_left, lower_right);
-            match escapes(Complex {re: point.0, im: point.1}, 255) {
-                None => {}, // This assumes the buffer is initialized to 0 and so skips this write
+    let mut offset: usize = 0;
+    for row in 0..bounds.1 {
+        for column in 0..bounds.0 {
+            let point = pixel_to_point(bounds, (column, row), upper_left, lower_right);
+            match escapes(point, 255) {
+                None => {} // This assumes the buffer is initialized to 0 and so skips this write
                 Some(count) => {
                     pixels[offset] = 255 - count as u8;
                 }
@@ -109,6 +152,17 @@ fn render(pixels: &mut [u8], bounds: (usize, usize),
             offset += 1;
         }
     }
+}
+
+/// Write the buffer 'pixels', whose dimensions are given by 'bounds', to the file named 'filename'
+fn write_bitmap(filename: &PathBuf, pixels: &[u8], bounds: (usize, usize)) -> Result<()> {
+    let output = File::create(filename)?;
+
+    let encoder = PNGEncoder::new(output);
+    encoder.encode(&pixels, bounds.0 as u32, bounds.1 as u32,
+                   ColorType::Gray(8))?;
+
+    Ok(())
 }
 
 #[cfg(test)]
@@ -119,8 +173,10 @@ mod tests {
 
     #[test]
     fn test_pixel_to_point() {
-        assert_eq!(pixel_to_point((100, 100), (25,75), (-1.0, 1.0), (1.0, -1.0)),
-                   (-0.5, -0.5));
+        assert_eq!(pixel_to_point((100, 100), (25, 75),
+                                  Complex { re: -1.0, im: 1.0 },
+                                  Complex { re: 1.0, im: -1.0 }),
+                   Complex { re: -0.5, im: -0.5 });
     }
 
     #[test]
@@ -128,31 +184,56 @@ mod tests {
         assert_eq!(parse_pair::<i32>("", ','), None);
         assert_eq!(parse_pair::<i32>("10,", ','), None);
         assert_eq!(parse_pair::<i32>(",10", ','), None);
-        assert_eq!(parse_pair::<i32>("10,20", ','), Some((10,20)));
+        assert_eq!(parse_pair::<i32>("10,20", ','), Some((10, 20)));
         assert_eq!(parse_pair::<i32>("10,20xy", ','), None);
         assert_eq!(parse_pair::<f64>("0.5x", ','), None);
         assert_eq!(parse_pair::<f64>("0.5x1.5", 'x'), Some((0.5, 1.5)));
     }
 
+    #[test]
+    fn test_parse_complex() {
+        assert_eq!(parse_complex("1.25,-0.0625"),
+                   Some(Complex { re: 1.25, im: -0.0625 }));
+        assert_eq!(parse_complex(",-0.0625"), None);
+    }
+
+    #[bench]
+    fn bench_escapes(b: &mut Bencher) {
+        let upper_left = Complex { re: -1.20, im: 0.35 };
+
+        b.iter(|| escapes(upper_left, 255).unwrap());
+    }
+
+    #[bench]
+    fn bench_pixel_to_point(b: &mut Bencher) {
+        let bounds = (1000, 1000);
+        let pixel = (500, 500);
+        let upper_left = Complex { re: -1.20, im: 0.35 };
+        let lower_right = Complex { re: -1.0, im: 0.20 };
+
+        b.iter(|| pixel_to_point(bounds, pixel, upper_left, lower_right));
+    }
+
     #[bench]
     fn bench_render_1000_by_1000(b: &mut Bencher) {
         let bounds = (1000, 1000);
-        let upper_left = (-1.20, 0.35);
-        let lower_right = (-1.0, 0.20);
+        let upper_left = Complex { re: -1.20, im: 0.35 };
+        let lower_right = Complex { re: -1.0, im: 0.20 };
         let mut pixels = vec![0; bounds.0 * bounds.1];
 
         b.iter(|| render(&mut pixels, bounds, upper_left, lower_right));
     }
 
     // TODO fix this test
-    #[test] #[ignore]
+    #[test]
+    #[ignore]
     fn compare_gold_masters() {
         let tmp_dir = TempDir::new("output_tests").expect("create temp dir failed");
         println!("Generating test files in {:?}", tmp_dir);
         let filename = tmp_dir.path().join("mandel_4000x3000.png");
         let bounds = (4000, 3000);
-        let upper_left = (-1.20, 0.35);
-        let lower_right = (-1.0, 0.20);
+        let upper_left = Complex { re: -1.20, im: 0.35 };
+        let lower_right = Complex { re: -1.0, im: 0.20 };
         let mut pixels = vec![0; bounds.0 * bounds.1];
 
         render(&mut pixels, bounds, upper_left, lower_right);
@@ -162,15 +243,4 @@ mod tests {
         // Compare output to the "golden master" file generated in the first version
         assert!(!dir_diff::is_different(&tmp_dir.path(), "gold_masters").unwrap());
     }
-}
-
-/// Write the buffer 'pixels', whose dimensions are given by 'bounds', to the file named 'filename'
-fn write_bitmap(filename: &PathBuf, pixels: &[u8], bounds: (usize, usize)) -> Result<()> {
-    let output = File::create(filename)?;
-
-    let encoder = PNGEncoder::new(output);
-    encoder.encode(&pixels, bounds.0 as u32, bounds.1 as u32,
-        ColorType::Gray(8))?;
-
-    Ok(())
 }
